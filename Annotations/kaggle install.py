9b0e3bb40393bec610749825e7d48d553bce3648
tanyarajput08 <tanyarajput121203@gmail.com>
2024-11-14 19:54:03 +0530

    Add files via upload

---
+++ Annotations/kaggle install.py
@@ -0,0 +1,7 @@
+pip install kaggle 
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("issaisasank/guns-object-detection")
+
+print("D:\\workspace\\python codes\\labelImg\\image dataset"", path")
